5bde4bf9a78f283200ef16dc6213c76a7ca3e23b
Artemyin <Artemyin@gmail.com>
2022-01-20 11:33:17 +0200

    Update __init__.py

--- department_app/models/__init__.py
+++ department_app/models/__init__.py
@@ -2,7 +2,8 @@ from .base import db, migrate
 
 
 def init_app(app):
-    """[summary]
+    """Registring database and migrations 
+    called from factory app id department_app.__init__.py
 
     :param app: [description]
     :type app: [type]
