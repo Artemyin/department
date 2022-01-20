d2bbcb74b947c2fdeab6a09994d14e6b7944c0a4
Artemyin <Artemyin@gmail.com>
2022-01-20 22:29:21 +0200

    PEP 8

--- department_app/models/__init__.py
+++ department_app/models/__init__.py
@@ -2,7 +2,7 @@ from .base import db, migrate
 
 
 def init_app(app):
-    """Registring database and migrations 
+    """Registring database and migrations
     called from factory app id department_app.__init__.py
     :param app: flask application
     :type app: Flask
