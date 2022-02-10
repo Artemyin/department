c2826f0b1611aaed2ea11ccb80a1274a075b38ce
Artemiy Noskov <Artemyin@gmail.com>
2022-02-10 19:11:37 +0000

    change SQLALCHEMY_DATABASE_URI to env variables

--- config.py
+++ config.py
@@ -9,22 +9,24 @@ class Config:
     DEBUG = False
     DEVELOPMENT = False
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
-    FLASK_APP = os.getenv("FLASK_APP", "app")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///department_db"
+    FLASK_APP = os.getenv("FLASK_APP", "department_app")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
+        "sqlite:///' + os.path.join(basedir, 'department_db')    
     DATABASE_CONNECT_OPTIONS = {}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_AUTO_RELOAD = True
 
 class ProductionConfig(Config):
-    """[summary]
-
+    """Production config
+    Use for production deployment
     """
     NAME = "Production"
     
 
 class DevelopmentConfig(Config):
-    """[summary]
-
+    """Development config
+    Use for development, DEBUG active
     """
     NAME = "Development"
     DEBUG = True
---
+++ department_app/test/__init__.py
@@ -0,0 +1,24 @@
+import os
+os.environ['DATABASE_URL'] = 'sqlite://'
+
+import unittest
+from flask import current_app
+from department_app import create_app
+
+
+class TestWebApp(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.appctx = self.app.app_context()
+        self.appctx.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.drop_all()
+        self.appctx.pop()
+        self.app = None
+        self.appctx = None
+
+    def test_app(self):
+        assert self.app is not None
+        assert current_app == self.app
